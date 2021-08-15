3c0ba519eb07197ae754c05ea676e137326ec36f
Kritsada Sunthornwutthikrai <kritsada.stw@gmail.com>
2021-08-15 13:44:10 +0700

    feat: Setup sveltekit and tailwind

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="text-yellow-600">
+    Tailwind text
+</div>
+
+<style>
+    @import "../assets/css/tailwind.css";
+</style>
